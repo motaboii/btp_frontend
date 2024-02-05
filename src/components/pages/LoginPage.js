0cadcb0d643a1ff701423ec29345cd777ada436e
NRS08 <nirantsontakke@gmail.com>
2024-02-05 15:12:38 +0530

    login functional

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,31 +1,63 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
-import './LoginPage.css'
+import "./LoginPage.css";
 
 export default function SignInPage() {
-    return (
-        <div className="text-center m-5-auto">
-            <h2>Sign in to us</h2>
-            <form action="/home">
-                <p>
-                    <label>Username or email address</label><br/>
-                    <input type="text" name="first_name" required />
-                </p>
-                <p>
-                    <label>Password</label>
-                    <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
-                    <br/>
-                    <input type="password" name="password" required />
-                </p>
-                <p>
-                    <button id="sub_btn" type="submit">Login</button>
-                </p>
-            </form>
-            <footer>
-                <p>First time? <Link to="/register">Create an account</Link>.</p>
-                <p><Link to="/">Back to Homepage</Link>.</p>
-            </footer>
-        </div>
-    )
+  const url = "https://misty-ray-threads.cyclic.app/api/v1/auth/login";
+  const navigate = useNavigate();
+
+  const login = async (e) => {
+    e.preventDefault();
+    try {
+      const email = document.querySelector("#email").value;
+      const password = document.querySelector("#Password").value;
+      const { data } = await axios.post(url, { email, password });
+      console.log(data);
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("scmName", data.user.name);
+      localStorage.setItem("scmRole", data.user.role);
+      navigate("/home");
+    } catch (error) {
+      localStorage.removeItem("token");
+      alert(error.response.data.msg);
+
+      document.querySelector("#email").value = "";
+    }
+  };
+
+  return (
+    <div className="text-center m-5-auto">
+      <h2>Sign in to us</h2>
+      <form>
+        <p>
+          <label>Username or email address</label>
+          <br />
+          <input id="email" type="text" name="first_name" required />
+        </p>
+        <p>
+          <label>Password</label>
+          <Link to="/forget-password">
+            <label className="right-label">Forget password?</label>
+          </Link>
+          <br />
+          <input id="Password" type="password" name="password" required />
+        </p>
+        <p>
+          <button onClick={(e) => login(e)} id="sub_btn">
+            Login
+          </button>
+        </p>
+      </form>
+      <footer>
+        <p>
+          First time? <Link to="/register">Create an account</Link>.
+        </p>
+        <p>
+          <Link to="/">Back to Homepage</Link>.
+        </p>
+      </footer>
+    </div>
+  );
 }
--- src/components/pages/OrgNavbar.js
+++ src/components/pages/OrgNavbar.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React, { useState, useEffect } from "react";
+
+import { Link, useNavigate } from "react-router-dom";
+import "./Navbar.css";
 
 const UserNavbar = () => {
+  const [name, setName] = useState("Name");
+  const navigate = useNavigate();
+  useEffect(() => {
+    const n = localStorage.getItem("scmName");
+    if (n) {
+      setName(n);
+    } else {
+      navigate("/orglogin");
+    }
+  });
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -19,6 +30,7 @@ const UserNavbar = () => {
           </Link>
         </div>
         <div>
+          <p className="usr">{name}</p>
           <Link to="/" className="dapp-link">
             Logout
           </Link>
